9857653abf01919471f65d550db1f07e3aa48d0b
Noah Hsu <i@nn.ci>
2022-10-30 14:52:18 +0800

    feat: use `lru-cache`

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,10 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import mem from "mem";
+import LRU from "lru-cache";
 
-type Data = {
-  // name: string;
-};
+const userCache = new LRU<string, GhUserUse[]>({
+  max: 500,
+  ttl: 1000 * 60 * 60 * 24,
+});
+
+const avatarCache = new LRU<string, string>({
+  max: 2000,
+  ttl: 1000 * 60 * 60 * 24,
+});
 
 type GhUser = {
   login: string;
@@ -26,7 +32,10 @@ type GhUser = {
   type: string;
   site_admin: boolean;
   contributions: number;
+  avatar_base64?: string;
 };
+
+type GhUserUse = Pick<GhUser, "login" | "avatar_url">;
 
 const getHead = (rows: number, cols: number) => {
   const width = 5 + cols * 69;
@@ -37,7 +46,7 @@ const getHead = (rows: number, cols: number) => {
 
 const foot = `\n</svg>`;
 
-const getUser = (row: number, col: number, user: GhUser) => {
+const getUser = (row: number, col: number, user: GhUser, avatar: string) => {
   const x = 5 + col * 69;
   const y = 5 + row * 69;
   return `<a xlink:href="https://github.com/${user.login}" class="link" target="_blank">
@@ -46,38 +55,48 @@ const getUser = (row: number, col: number, user: GhUser) => {
       <rect width="64" height="64" x="${x}" y="${y}" rx="32" />
     </clipPath>
   </defs>
-  <image class="avatar" clip-path="url(#${user.login})" x="${x}" y="${y}" width="64" height="64" xlink:href="data:image/png;base64,${user.avatar_url}" />
+  <image class="avatar" clip-path="url(#${user.login})" x="${x}" y="${y}" width="64" height="64" xlink:href="data:image/png;base64,${avatar}" />
 </a>`;
 };
 
 const fetchAvatar = async (url: string) => {
+  if (!url.startsWith("http")) {
+    return url;
+  }
+  if (avatarCache.has(url)) {
+    return avatarCache.get(url)!;
+  }
   const response = await fetch(url);
-  return Buffer.from(await response.arrayBuffer()).toString("base64");
+  const res = Buffer.from(await response.arrayBuffer()).toString("base64");
+  avatarCache.set(url, res);
+  return res;
 };
 
 const fetchRepo = async (repo: string) => {
+  if (userCache.has(repo)) {
+    return userCache.get(repo)!;
+  }
   console.log(`fetching ${repo}`);
   const res = await fetch(`https://api.github.com/repos/${repo}/contributors`);
   const users = await res.json();
   if (users.message) {
     throw new Error(`failed to fetch repo ${repo}: ${users.message}`);
   }
-  return users as GhUser[];
+  const usersUse = (users as GhUser[]).map((user) => {
+    return {
+      login: user.login,
+      avatar_url: user.avatar_url,
+    };
+  });
+  userCache.set(repo, usersUse);
+  return usersUse;
 };
-
-const memFetchRepo = mem(fetchRepo, {
-  maxAge: 1000 * 60 * 60 * 24,
-});
-
-const memFetchAvatar = mem(fetchAvatar, {
-  maxAge: 1000 * 60 * 60 * 24,
-});
 
 const fetchRepos = async (repos: string[]) => {
   const users = (
     await Promise.all(
       repos.map(async (repo) => {
-        const users = await memFetchRepo(repo);
+        const users = await fetchRepo(repo);
         return users;
       })
     )
@@ -86,18 +105,12 @@ const fetchRepos = async (repos: string[]) => {
     .filter((item, index, arr) => {
       return arr.findIndex((t) => t.login === item.login) === index;
     });
-  await Promise.all(
-    users.map(
-      async (user: GhUser) =>
-        (user.avatar_url = await memFetchAvatar(user.avatar_url))
-    )
-  );
   return users;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   try {
     const repos = [];
@@ -111,11 +124,14 @@ export default async function handler(
       throw new Error("repo is required");
     }
     const users = (await fetchRepos(repos)) as GhUser[];
+    const avatars = await Promise.all(
+      users.map(async (user: GhUserUse) => await fetchAvatar(user.avatar_url))
+    );
     const cols = parseInt((req.query.cols as string) ?? "12");
     const rows = Math.ceil(users.length / cols);
     let svg = getHead(rows, cols);
     for (let i = 0; i < users.length; i++) {
-      svg += getUser(Math.floor(i / cols), i % cols, users[i]);
+      svg += getUser(Math.floor(i / cols), i % cols, users[i], avatars[i]);
     }
     svg += foot;
     res
@@ -124,8 +140,13 @@ export default async function handler(
       .status(200)
       .send(svg);
   } catch (e: any) {
-    res.setHeader("Content-Type", "image/svg+xml").status(500)
-      .send(`<svg xmlns="http://www.w3.org/2000/svg">
+    res
+      .setHeader("Content-Type", "image/svg+xml")
+      .setHeader(
+        "Cache-Control",
+        "max-age=0, no-cache, no-store, must-revalidate"
+      )
+      .status(500).send(`<svg xmlns="http://www.w3.org/2000/svg">
       <foreignObject width="400" height="400">
         <body xmlns="http://www.w3.org/1999/xhtml">
           <p style="color: red;">${e.message}</p>
