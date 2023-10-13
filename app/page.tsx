bf149cf803f9a8d05480a0c43b4e898f8c9a90d0
Andy Hsu <i@nn.ci>
2023-10-13 17:58:19 +0800

    chore: uppercase title

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4 py-4">
       <div className="inline-block max-w-lg text-center justify-center">
         <h1 className={title()}>Github&nbsp;</h1>
-        <h1 className={title({ color: "indigo" })}>contributors&nbsp;</h1>
+        <h1 className={title({ color: "indigo" })}>Contributors&nbsp;</h1>
         <br />
-        <h1 className={title()}>SVG generator&nbsp;</h1>
+        <h1 className={title()}>SVG Generator&nbsp;</h1>
       </div>
       <Builder />
     </section>
