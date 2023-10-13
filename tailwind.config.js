c30e1dbcdbb3377aad78fe2e4b5448dc191b882a
Andy Hsu <i@nn.ci>
2023-10-13 18:38:13 +0800

    fix: tailwindcss does not take effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/react"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
